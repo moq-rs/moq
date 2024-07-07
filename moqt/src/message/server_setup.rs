0489bb0833659dfd389fbd4fc203be2d92983288
yngrtc <y@ngr.tc>
2024-07-07 15:29:21 -0700

    add test_server_setup

--- moqt/src/message/server_setup.rs
+++ moqt/src/message/server_setup.rs
@@ -38,3 +38,36 @@ impl Serializer for ServerSetup {
         Ok(l)
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::message::Message;
+    use std::io::Cursor;
+
+    #[test]
+    fn test_server_setup() -> Result<()> {
+        let raw_packet: Vec<u8> = vec![
+            0x40, 0x41, // type
+            192, 0, 0, 0, 255, 0, 0, 1,    // version Draft01
+            0x01, // one param
+            0x00, 0x01, 0x03, // role = PubSub
+        ];
+
+        let server_setup = Message::ServerSetup(ServerSetup {
+            supported_version: Version::Draft01,
+            role: Role::PubSub,
+        });
+
+        let mut cursor: Cursor<&[u8]> = Cursor::new(raw_packet.as_ref());
+        let (actual_server_setup, actual_len) = Message::deserialize(&mut cursor)?;
+        assert_eq!(server_setup, actual_server_setup);
+        assert_eq!(raw_packet.len(), actual_len);
+
+        let mut actual_packet = vec![];
+        let _ = server_setup.serialize(&mut actual_packet)?;
+        assert_eq!(raw_packet, actual_packet);
+
+        Ok(())
+    }
+}
